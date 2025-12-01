c0913de1257aea924194515daeaa96acd8afdea7
DavidBewley <davidmartinbewley@gmail.com>
2025-12-01 11:18:49 +0000

    Day1

---
+++ AdventOfCode25/Day1.cs
@@ -0,0 +1,55 @@
+﻿using Shouldly;
+
+namespace AdventOfCode25
+{
+    public class Day1
+    {
+        private string _inputString;
+        public Day1()
+        {
+            _inputString = File.ReadAllText("Inputs\\Day1.txt");
+        }
+
+        [Theory]
+        [InlineData("R50", 1)]
+        [InlineData("L50", 1)]
+        [InlineData("L50\r\nL100\r\nR100", 3)]
+        [InlineData("L68\r\nL30\r\nR48\r\nL5\r\nR60\r\nL55\r\nL1\r\nL99\r\nR14\r\nL82", 3)]
+        public void TimesZero(string input, int expectedOutput)
+        {
+            var processor = new Logic.Day1Processor(50);
+            var result = processor.ProcessLeftTurns(input, 1);
+            result.ShouldBe(expectedOutput);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var processor = new Logic.Day1Processor(50);
+            var result = processor.ProcessLeftTurns(_inputString, 1);
+            result.ShouldBe(1129);
+        }
+
+
+        [Theory]
+        [InlineData("R100\r\nR100", 2)]
+        [InlineData("L100\r\nL100", 2)]
+        [InlineData("R50\r\nL50\r\nL50\r\nR50\r\nR50\r\nR100", 4)]
+        [InlineData("R100\r\nR200\r\nL300", 6)]
+        [InlineData("L68\r\nL30\r\nR48\r\nL5\r\nR60\r\nL55\r\nL1\r\nL99\r\nR14\r\nL82", 6)]
+        public void TimesZeroPart2(string input, int expectedOutput)
+        {
+            var processor = new Logic.Day1Processor(50);
+            var result = processor.ProcessLeftTurns(input, 2);
+            result.ShouldBe(expectedOutput);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var processor = new Logic.Day1Processor(50);
+            var result = processor.ProcessLeftTurns(_inputString, 2);
+            result.ShouldBe(6638);
+        }
+    }
+}
---
+++ Logic/Day1Processor.cs
@@ -0,0 +1,93 @@
+﻿namespace Logic
+{
+    public class Day1Processor
+    {
+
+        private int _startingNumber;
+        private int _currentNumber;
+        private int _timesZeroPart2 = 0;
+        private int _timesZeroPart1 = 0;
+
+        public Day1Processor(int startingNumber)
+        {
+            _startingNumber = startingNumber;
+        }
+
+        public int ProcessLeftTurns(string input, int returnType)
+        {
+            _currentNumber = _startingNumber;
+
+            var instructions = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var instruction in instructions)
+            {
+                var direction = instruction[0];
+                var numberOfTurns = int.Parse(instruction[1..]);
+                if (direction == 'L')
+                    TurnLeft(numberOfTurns);
+                else if (direction == 'R')
+                    TurnRight(numberOfTurns);
+                if (_currentNumber == 0)
+                    _timesZeroPart1++;
+            }
+
+            if (returnType == 1)
+                return _timesZeroPart1;
+            return _timesZeroPart2;
+        }
+
+        private void TurnLeft(int numberOfTurns)
+        {
+            _currentNumber = _currentNumber - numberOfTurns;
+            while (_currentNumber < 0)
+            {
+                _currentNumber += 100;
+                _timesZeroPart2++;
+            }
+        }
+
+        private void TurnRight(int numberOfTurns)
+        {
+            _currentNumber = _currentNumber + numberOfTurns;
+            while (_currentNumber > 99)
+            {
+                _currentNumber -= 100;
+                _timesZeroPart2++;
+            }
+        }
+
+        public int BruteForcePart2(string input)
+        {
+            var instructions = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var instruction in instructions)
+            {
+                var direction = instruction[0];
+                var numberOfTurns = int.Parse(instruction[1..]);
+                if (direction == 'R')
+                {
+                    for (int i = 0; i < numberOfTurns; i++)
+                    {
+                        if (_currentNumber == 99)                       
+                            _currentNumber = 0;
+                        else
+                            _currentNumber++;
+                        if (_currentNumber == 0)
+                            _timesZeroPart2++;
+                    }                   
+                }
+                else
+                {
+                    for (int i = 0; i < numberOfTurns; i++)
+                    {
+                        if (_currentNumber == 0)
+                            _currentNumber = 99;
+                        else
+                            _currentNumber--;
+                        if (_currentNumber == 0)
+                            _timesZeroPart2++;
+                    }
+                }
+            }
+            return _timesZeroPart2;
+        }
+    }
+}
