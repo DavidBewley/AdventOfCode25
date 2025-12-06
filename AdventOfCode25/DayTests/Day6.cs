6796fc55c5f3203b5ac45b6ec4768801e007a538
DavidBewley <davidmartinbewley@gmail.com>
2025-12-06 16:03:24 +0000

    Day 6

---
+++ AdventOfCode25/DayTests/Day6.cs
@@ -0,0 +1,52 @@
+﻿using Shouldly;
+
+namespace AdventOfCode25.DayTests
+{
+    public class Day6
+    {
+        private string _inputString;
+        public Day6() => _inputString = File.ReadAllText($"Inputs\\{this.GetType().Name}.txt");
+
+        [Theory]
+        [InlineData("123\r\n 45 \r\n  6\r\n*", 33210)]
+        [InlineData("328\r\n 64 \r\n 98\r\n+", 490)]
+        [InlineData(" 51\r\n387\r\n215\r\n*", 4243455)]
+        [InlineData(" 64 \r\n23\r\n314\r\n+", 401)]
+        [InlineData("123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   + ", 4277556)]
+        public void Part1Theroy(string input, double expectedOutput)
+        {
+            var processor = new Logic.Day6Processor();
+            var result = processor.Part1(input);
+            result.ShouldBe(expectedOutput);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var processor = new Logic.Day6Processor();
+            var result = processor.Part1(_inputString);
+            result.ShouldBe(6371789547734);
+        }
+
+        [Theory]
+        [InlineData("64 \r\n23 \r\n314\r\n+  ", 1058)]
+        [InlineData(" 51\r\n387\r\n215\r\n*  ", 3253600)]
+        [InlineData("328\r\n64 \r\n98 \r\n+  ", 625)]
+        [InlineData("123\r\n 45\r\n  6\r\n*  ", 8544)]
+        [InlineData("123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +   ", 3263827)]
+        public void Part2Theroy(string input, int expectedOutput)
+        {
+            var processor = new Logic.Day6Processor();
+            var result = processor.Part2(input);
+            result.ShouldBe(expectedOutput);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var processor = new Logic.Day6Processor();
+            var result = processor.Part2(_inputString);
+            result.ShouldBe(11419862653216);
+        }
+    }
+}
---
+++ Logic/Day6Processor.cs
@@ -0,0 +1,105 @@
+﻿namespace Logic
+{
+    public class Day6Processor
+    {
+        public double Part1(string input)
+        {
+            var equations = GetAllEquations(input);
+            var total = 0d;
+
+            foreach (var (op, items) in equations)
+            {
+                double result = op switch
+                {
+                    "+" => items.Sum(),
+                    "*" => items.Aggregate(1d, (a, b) => a * b),
+                    _ => 0d
+                };
+                total += result;
+            }
+
+            return total;
+        }
+
+        public double Part2(string input)
+        {
+            var equations = GetAllEquationsPart2(input);
+            var total = 0d;
+
+            foreach (var (op, items) in equations)
+            {
+                double result = op switch
+                {
+                    "+" => items.Sum(),
+                    "*" => items.Aggregate(1d, (a, b) => a * b),
+                    _ => 0d
+                };
+                total += result;
+            }
+
+            return total;
+        }
+
+        private List<(string op, List<double> items)> GetAllEquations(string input)
+        {
+            var equations = new List<(string op, List<double> items)>();
+            var lines = input.Split('\n');
+            var operators = lines.Last().ToString().Split(' ').ToList();
+            operators.RemoveAll(s => string.IsNullOrWhiteSpace(s));
+
+            foreach (var op in operators)
+                equations.Add(new(op, []));
+
+            for (var i = 0; i < lines.Length - 1; i++)
+            {
+                var items = lines[i].ToString().Split(' ').ToList();
+                items.RemoveAll(s => string.IsNullOrWhiteSpace(s));
+                for (var j = 0; j < items.Count; j++)
+                    equations[j].items.Add(double.Parse(items[j]));
+            }
+
+            return equations;
+        }
+
+        private List<(string op, List<double> items)> GetAllEquationsPart2(string input)
+        {
+            var equations = new List<(string op, List<double> items)>();
+            var lines = input.Split(["\r\n", "\r", "\n"], StringSplitOptions.None);
+            var op = "";
+            var currentNumbers = new List<double>();
+
+
+            for (var i = lines.First().Length - 1; i >= 0; i--)
+            {
+                var line = "";
+                for (var k = 0; k < lines.Length; k++)
+                    line += lines[k][i];
+
+                if(string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                if (line.Contains("*"))
+                {
+                    op = "*";
+                    line = line.Replace("*", "");
+                }
+                if (line.Contains("+"))
+                {
+                    op = "+";
+                    line = line.Replace("+", "");
+                }
+                if (!string.IsNullOrWhiteSpace(line))
+                    currentNumbers.Add(double.Parse(line));
+                if (op != "")
+                {
+                    equations.Add((op, currentNumbers));
+                    currentNumbers = new List<double>();
+                    op = "";
+                }
+
+            }
+
+            return equations;
+        }
+    }
+}
