fc5a89c6cc2193ac17fc38c3b10d9b0cd0bb1b9c
DavidBewley <davidmartinbewley@gmail.com>
2025-12-07 12:49:38 +0000

    Day 7

---
+++ AdventOfCode25/DayTests/Day7.cs
@@ -0,0 +1,49 @@
+﻿using Shouldly;
+
+namespace AdventOfCode25.DayTests
+{
+    public class Day7
+    {
+        private string _inputString;
+        public Day7() => _inputString = File.ReadAllText($"Inputs\\{this.GetType().Name}.txt");
+
+        [Theory]
+        [InlineData("..S..\r\n.....\r\n.....\r\n.....\r\n^.^.^\r\n.....", 1)]
+        [InlineData("..S..\r\n.....\r\n..^..\r\n.....\r\n^.^.^\r\n.....", 1)]
+        [InlineData("..S..\r\n.....\r\n..^..\r\n.....\r\n.^..^\r\n.....", 2)]
+        [InlineData("..S..\r\n.....\r\n..^..\r\n.....\r\n.^.^.\r\n.....", 3)]
+        [InlineData(".......S.......\r\n...............\r\n.......^.......\r\n...............\r\n......^.^......\r\n...............\r\n.....^.^.^.....\r\n...............\r\n....^.^...^....\r\n...............\r\n...^.^...^.^...\r\n...............\r\n..^...^.....^..\r\n...............\r\n.^.^.^.^.^...^.\r\n...............", 21)]
+        public void Part1Theroy(string input, double expectedOutput)
+        {
+            var processor = new Logic.Day7Processor();
+            var result = processor.Part1(input);
+            result.ShouldBe(expectedOutput);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var processor = new Logic.Day7Processor();
+            var result = processor.Part1(_inputString);
+            result.ShouldBe(1678);
+        }
+
+        [Theory]
+        [InlineData("..S..\r\n.....\r\n..^..\r\n.....\r\n.^.^.\r\n.....", 4)]
+        [InlineData(".......S.......\r\n...............\r\n.......^.......\r\n...............\r\n......^.^......\r\n...............\r\n.....^.^.^.....\r\n...............\r\n....^.^...^....\r\n...............\r\n...^.^...^.^...\r\n...............\r\n..^...^.....^..\r\n...............\r\n.^.^.^.^.^...^.\r\n...............", 40)]
+        public void Part2Theroy(string input, int expectedOutput)
+        {
+            var processor = new Logic.Day7Processor();
+            var result = processor.Part2(input);
+            result.ShouldBe(expectedOutput);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var processor = new Logic.Day7Processor();
+            var result = processor.Part2(_inputString);
+            result.ShouldBe(357525737893560);
+        }
+    }
+}
---
+++ Logic/Day7Processor.cs
@@ -0,0 +1,94 @@
+﻿namespace Logic
+{
+    public class Day7Processor
+    {
+        public double Part1(string input)
+        {
+            var lines = input.Split(["\r\n", "\r", "\n"], StringSplitOptions.None);
+            var beamPositions = new List<int>();
+            var splitCount = 0;
+            beamPositions.Add(lines[0].IndexOf('S'));
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                if (!lines[i].Contains('^'))
+                    continue;
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (lines[i][j] == '^' && beamPositions.Contains(j))
+                    {
+                        beamPositions.Remove(j);
+                        if (!beamPositions.Contains(j - 1))
+                            beamPositions.Add(j - 1);
+                        if (!beamPositions.Contains(j + 1))
+                            beamPositions.Add(j + 1);
+                        splitCount++;
+                    }
+                }
+            }
+
+            return splitCount;
+        }
+
+        //.......S.......
+        //...............
+        //.......^.......
+        //...............
+        //......^.^......
+        //...............
+        //.....^.^.^.....
+        //...............
+        //....^.^...^....
+        //...............
+        //...^.^...^.^...
+        //...............
+        //..^...^.....^..
+        //...............
+        //.^.^.^.^.^...^.
+        //...............
+
+        //Track beam positions as a list.
+        //loop through the rows, for each row, find the beam positions if it splits
+
+
+        public double Part2(string input)
+        {
+            var lines = input.Split(["\r\n", "\r", "\n"], StringSplitOptions.None);
+            var beamPositions = new List<int>();
+            beamPositions.Add(lines[0].IndexOf('S'));
+            var timelines = new Dictionary<int, double>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                if (!lines[i].Contains('^'))
+                    continue;
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (lines[i][j] == '^' && beamPositions.Contains(j))
+                    {
+                        beamPositions.Remove(j);
+
+                        if (timelines.ContainsKey(j - 1))
+                            timelines[j - 1] += timelines[j];
+                        else
+                            timelines[j - 1] = 1;
+
+                        if (timelines.ContainsKey(j + 1))
+                            timelines[j + 1] += timelines[j];
+                        else
+                            timelines[j + 1] = 1;
+
+                        timelines[j] = 0;
+
+                        if (!beamPositions.Contains(j + 1))
+                            beamPositions.Add(j + 1);
+                        if (!beamPositions.Contains(j - 1))
+                            beamPositions.Add(j - 1);
+
+                    }
+                }
+            }
+            return timelines.Sum(t => t.Value);
+        }
+    }
+}
